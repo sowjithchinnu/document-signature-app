9474d422ae81d6c9e36259436ab9460e6372ded6
Nalli Sowjith Kumar <24bec030@iiitdwd.ac.in>
2026-06-05 22:20:58 +0530

    Day 2: JWT authentication completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authRoutes);
+
 app.get("/", (req, res) => {
   res.send("Document Signature API Running");
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
